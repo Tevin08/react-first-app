2dbc879f94d28c76f42eaf3375bdd1b2233b712c
estevao <estevaopsg08@gmail.com>
2023-11-17 23:35:02 -0300

    rescue

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,36 @@ import ButtonTrocaFundo from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [random,setRandom] = useState(Math.floor(Math.random(4)));
   const [cor, setCor] = useState(false);
   function trocarFundo() {
     setCor(!cor);
+    setRandom(Math.floor(Math.random()*4))
   }
+  const colors = [
+    'bg-green-500',
+    'bg-blue-500',
+    'bg-slate-500',
+    'bg-black-500',
+    
+
+  ]
   return (
     <main
       className={`flex flex-col justify-center text-center ${
-        cor ? "bg-red-500" : null
+         colors[random] 
+       
       } min-h-screen`}
     >
-      <div className="flex justify-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </button> */}
+        
       </div>
-      <p className="text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+     
       <ButtonTrocaFundo trocarFundo={trocarFundo} />
     </main>
   );
